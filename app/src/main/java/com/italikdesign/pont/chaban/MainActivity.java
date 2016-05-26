58e3684ae4d158d55e604eaeb5e5f7886d8780a7
marlenech <italikdesignbordeaux@gmail.com>
2016-05-26 22:39:41 +0200

    Delete Floating button

--- app/src/main/java/com/italikdesign/pont/chaban/MainActivity.java
+++ app/src/main/java/com/italikdesign/pont/chaban/MainActivity.java
@@ -38,14 +38,7 @@ public class MainActivity extends AppCompatActivity
         toolbar.setTitle("Le Pont Chaban Delmas");
         toolbar.setSubtitle("Dates et Heures de Levées");
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 
