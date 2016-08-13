d58b1d827478b35920c6f28e26b6e787a4ed514d
marlenech <italikdesignbordeaux@gmail.com>
2016-08-13 16:10:04 +0200

    Creating the view aPropos

--- app/src/main/java/com/italikdesign/pont/chaban/AproposFragment.java
+++ app/src/main/java/com/italikdesign/pont/chaban/AproposFragment.java
@@ -5,10 +5,13 @@ package com.italikdesign.pont.chaban;
  */
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 
 /**
@@ -17,7 +20,7 @@ import android.widget.ProgressBar;
 public class AproposFragment extends Fragment {
 
     private View rootView;
-    private ProgressBar progressBar;
+
 
     public AproposFragment() {
         // Required empty public constructor
@@ -30,8 +33,20 @@ public class AproposFragment extends Fragment {
 
         rootView = inflater.inflate(R.layout.fragment_apropos, container, false);
         // Inflate the layout for this fragment
-        progressBar = (ProgressBar) rootView.findViewById(R.id.progress_bar);
-        progressBar.setVisibility(View.VISIBLE);
+
+        TextView feedback = (TextView) rootView.findViewById(R.id.contact);
+        feedback.setText(Html.fromHtml("<a href=\"mailto:italikdesignbordeaux@gmail.com\">italikdesignbordeaux@gmail.com</a>"));
+        feedback.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView feedback1 = (TextView) rootView.findViewById(R.id.lien_site1);
+        feedback1.setText(Html.fromHtml("<a href=\"http://sedeplacer.bordeaux-metropole.fr/Toutes-les-infos-circulation/Pont-Chaban-Delmas-Fermetures\">http://sedeplacer.bordeaux-metropole.fr</a>"));
+        feedback1.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView feedback2 = (TextView) rootView.findViewById(R.id.lien_opendata);
+        feedback2.setText(Html.fromHtml("<a href=\"http://data.bordeaux-metropole.fr/data.php?layer=PREVISIONS_PONT_CHABAN\">http://data.bordeaux-metropole.fr</a>"));
+        feedback2.setMovementMethod(LinkMovementMethod.getInstance());
+
+
 
         return rootView;
 
